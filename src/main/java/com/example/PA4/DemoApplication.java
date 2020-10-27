d45d299f27b4b60575eb4a086ef6570f6dead505
Manu <lopez.emanuela@gmail.com>
2020-10-27 15:47:34 -0300

    ADD Java 11 e conexão

--- src/main/java/com/example/PA4/DemoApplication.java
+++ src/main/java/com/example/PA4/DemoApplication.java
@@ -1,15 +1,21 @@
 package com.example.PA4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 
 		SpringApplication.run(DemoApplication.class, args);
 
-		
+		DriverManager.getConnection("jdbc:mysql://localhost/Doctor","adminManu","adminManu");
+        System.out.println("Conectado!");
+
 	}
 
 }
