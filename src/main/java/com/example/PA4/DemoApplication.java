1681ca828934dcf3179fa3364b6d9041ee79c03b
Manu <lopez.emanuela@gmail.com>
2020-10-29 20:36:45 -0300

    29/10

--- src/main/java/com/example/PA4/DemoApplication.java
+++ src/main/java/com/example/PA4/DemoApplication.java
@@ -1,20 +1,13 @@
 package com.example.PA4;
 
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication{
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args){
 
 		SpringApplication.run(DemoApplication.class, args);
-
-		DriverManager.getConnection("jdbc:mysql://15.0.2000:3306/Doctor","adminManu","adminManu");
-        System.out.println("Conectado!");
 
 	}
 
---
+++ src/main/java/com/example/PA4/Entidade/Usuario.java
@@ -0,0 +1,11 @@
+package com.example.PA4.Entidade;
+
+public class Usuario
+{
+    private int id;
+    private String nome;
+    private Date birth;
+    private String genero;
+    private String username;
+    private String password;
+}
